8440fd1ba409cbe1cf91c052bfc875fb614bf8b2
sswyya <sawanya.samol@gmail.com>
2023-12-12 21:45:08 +0700

    Your commit message here

---
+++ src/app/register.tsx
@@ -0,0 +1,8 @@
+ 
+export default function register() {
+    return (
+        <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+        Learn about Next.js in an interactive course with&nbsp;quizzes!
+      </p>
+        )
+    }
